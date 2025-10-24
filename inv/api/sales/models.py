9a7ce4429162ee21191d69758a1695b8ef4052fa
ronnin796 <nischal.chaudhary796@gmail.com>
2025-10-24 08:48:57 +0545

    date editable of sale item

--- inv/api/sales/models.py
+++ inv/api/sales/models.py
@@ -34,7 +34,7 @@ class SalesItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.DecimalField(max_digits=12, decimal_places=2)
     price = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         ordering = ["id"]
